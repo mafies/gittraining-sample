8212a23fd367b9594cc2d93d31daab6d1f1695c8
Markus Feißt <mfeisst@meyle-mueller.de>
2013-04-08 14:24:15 +0200

    Add power operation
    Power operation was added....

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -17,6 +17,7 @@ import org.eclipse.example.calc.internal.operations.Equals;
 import org.eclipse.example.calc.internal.operations.Minus;
 import org.eclipse.example.calc.internal.operations.Plus;
 import org.eclipse.example.calc.internal.operations.Square;
+import org.eclipse.example.calc.internal.operations.Power;
 
 public class Calculator {
 
@@ -40,6 +41,7 @@ public class Calculator {
 		new Minus();
 		new Plus();
 		new Square();
+		new Power();
 	}
 
 	private void calculate(String cmdName) {
